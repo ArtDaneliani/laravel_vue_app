8a50527473e802ea1444e094a5328c2529288858
artDaneliani <nuclear_down@yahoo.com>
2023-02-28 09:20:38 +0300

    test

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -45,27 +45,21 @@ class ProductController extends Controller
         $tags = $data['tags'];
         unset($data['tags']);
 //        dd($tags, $data);
-
         $product = Product::create($data);
         $product->tags()->attach($tags);
-
-
         return redirect()->route('product.index');
     }
-
     public function show(Product $product)
     {
         $tags = Tag::all();
         return view('product.show', compact('product', 'tags'));
     }
-
     public function edit(Product $product)
     {
         $categories = Category::all();
         $tags = Tag::all();
         return view('product.edit', compact('product', 'categories', 'tags'));
     }
-
     public function update(Product $product)
     {
         $data = request()->validate([
@@ -81,45 +75,33 @@ class ProductController extends Controller
         ]);
         $tags = $data['tags'];
         unset($data['tags']);
-
         $product->update($data);
         $product->tags()->sync($tags);
-
         return redirect()->route('product.show', $product->id);
     }
-
     public function destroy(Product $product)
     {
         $product->delete();
         return redirect()->route('product.index');
     }
-
     /*-----------------END CRUD------------------*/
-
     public function image_upload(Request $request)
     {
         $validatedData = $request->validate([
             'userfile' => 'image',
             'userfile.*' => 'mimetypes:image/jpeg,image/png,image/gif,image/webp',
         ]);
-
-
         if ($request->hasfile('images')) {
             foreach ($request->file('images') as $key => $image) {
                 $path = 'images';
                 $name = $image->getClientOriginalName();
-
 //                $insert[$key]['name'] = $name;
 //                $insert[$key]['path'] = $path;
-
                 Storage::putFileAs($path, $image, $name);
             }
         }
-
 //        Product::insert($insert);
-
         return redirect()->route('product.index')->with('status', 'Несколько файлов загружены успешно');
-
 //        if ($request->isMethod('post') && $request->file('userfile')) {
 //            $request->validate([
 //                // файл должен быть картинкой (jpeg, png, bmp, gif, svg или webp)
