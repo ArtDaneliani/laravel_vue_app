3aff2791bf8807f5af3aac5fd049e0d4948bdbb6
artDaneliani <nuclear_down@yahoo.com>
2023-04-15 01:27:29 +0300

    ToDo list. Part-3 (Auth + Vue-crud)

---
+++ resources/views/home1VERs.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('title')Главная @endsection()
+
+@section('main_content')
+    <h1>Главная</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Accusamus amet consequatur dolores harum, inventore iure saepe totam.
+        Architecto dolorem iste,
+        libero nesciunt perferendis placeat quas rerum soluta sunt veniam, voluptas.
+    </p>
+    <home>
+@endsection()
